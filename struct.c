fdb5621680ac88fb13f423ac4782f14d39090438
Usaid <usaidokdev@gmail.com>
2025-08-26 14:39:16 +0530

    Add contact completed

--- struct.c
+++ struct.c
@@ -8,8 +8,26 @@ struct Contact
     char phone[15];
     char email[25];
 };
+ void Addnewcontact(){
+    struct Contact Contact1;
+    printf("Please enter your name:");
+    getchar();
+    fgets(Contact1.name, sizeof(Contact1.name), stdin);
+    Contact1.name[strcspn(Contact1.name, "\n")] = '\0';
+
+    printf("Enter your phone:-");
+    scanf("%14s",Contact1.phone);
+    printf("Enter your email:-");
+    scanf(" %20s", Contact1.email);
+    printf("Contact add successfully!");
+
+    // printf("----Saved Contacts----\n");
+    // printf("Name:-%s\n", Contact1.name);
+    // printf("Phone:-%s\n", Contact1.phone);
+    // printf("Email:-%s\n", Contact1.email);
+ }
  void Displaymenu(){
-    int choise=0;
+    int choise;
     printf("--Contact Management System---\n");
     printf("1:Add new contact\n");
     printf("2:View all contact\n");
@@ -18,29 +36,23 @@ struct Contact
     printf("5:Delete a contact\n");
     printf("6:Exit the program\n");
 
-    printf("\nChoose the number:");
-    
-    scanf("%d", &choise);
+    printf("\nChoose a number:");
+    scanf("%d",&choise);
+    switch (choise)
+    {
+    case 1:
+        Addnewcontact();
+        break;
+    case 2:
+        break;
+    default:
+        break;
+    }
 
  }
 
 int main(){
-    struct Contact Contact1;
-    printf("Please enter your name:");
-    fgets(Contact1.name, sizeof(Contact1.name), stdin);
-    Contact1.name[strcspn(Contact1.name, "\n")] = '\0';
-
-    printf("Enter your phone:-");
-    scanf("%14s",Contact1.phone);
-    printf("Enter your email:-");
-    scanf(" %20s", Contact1.email);
-
-    printf("----Saved Contacts----\n");
-    printf("Name:-%s\n", Contact1.name);
-    printf("Phone:-%s\n", Contact1.phone);
-    printf("Email:-%s\n", Contact1.email);
     Displaymenu();
-
 
     return 0;
 }
