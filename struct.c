ad5c843f0640cbdbe2a15f9e0785762f01e1005d
Usaid <usaidokdev@gmail.com>
2025-08-27 11:09:25 +0530

    Contact added successfull

--- struct.c
+++ struct.c
@@ -8,51 +8,86 @@ struct Contact
     char phone[15];
     char email[25];
 };
- void Addnewcontact(){
-    struct Contact Contact1;
-    printf("Please enter your name:");
-    getchar();
-    fgets(Contact1.name, sizeof(Contact1.name), stdin);
-    Contact1.name[strcspn(Contact1.name, "\n")] = '\0';
 
-    printf("Enter your phone:-");
-    scanf("%14s",Contact1.phone);
-    printf("Enter your email:-");
-    scanf(" %20s", Contact1.email);
-    printf("Contact add successfully!");
+// Global variables
+struct Contact Contacts[100];
+int Contactcount = 0;
 
-    // printf("----Saved Contacts----\n");
-    // printf("Name:-%s\n", Contact1.name);
-    // printf("Phone:-%s\n", Contact1.phone);
-    // printf("Email:-%s\n", Contact1.email);
- }
- void Displaymenu(){
-    int choise;
-    printf("--Contact Management System---\n");
-    printf("1:Add new contact\n");
-    printf("2:View all contact\n");
-    printf("3:Search for a contact\n");
-    printf("4:Modify a contact\n");
-    printf("5:Delete a contact\n");
-    printf("6:Exit the program\n");
-
-    printf("\nChoose a number:");
-    scanf("%d",&choise);
-    switch (choise)
+void ClearInputBuffer()
+{
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF)
     {
-    case 1:
-        Addnewcontact();
-        break;
-    case 2:
-        break;
-    default:
-        break;
+    }
+}
+// Add contacts function
+void Addnewcontact()
+{
+    printf("Name: ");
+    ClearInputBuffer();
+    fgets(Contacts[Contactcount].name, sizeof(Contacts[Contactcount].name), stdin);
+    Contacts[Contactcount].name[strcspn(Contacts[Contactcount].name, "\n")] = '\0';
+
+    printf("Phone: ");
+    scanf("%14s", Contacts[Contactcount].phone);
+
+    printf("Email: ");
+    scanf("%24s", Contacts[Contactcount].email);
+
+    printf("Contact added!");
+    Contactcount++;
+}
+
+// View contact function
+void Viewcontact()
+{
+    if (Contactcount == 0)
+    {
+        printf("No contact added!!\n");
+        return;
     }
 
- }
+    for (int i = 0; i < Contactcount; i++)
+    {
+        printf("Contact #%d\n", i + 1);
+        printf("Name: %s\n", Contacts[i].name);
+        printf("Phone: %s\n", Contacts[i].phone);
+        printf("Email: %s\n", Contacts[i].email);
+        printf("-----------------\n");
+    }
+}
 
-int main(){
+void Displaymenu()
+{
+    int choise;
+    while (1)
+    {
+        printf("\n--Contact Management System---\n");
+        printf("1:Add new contact\n");
+        printf("2:View all contact\n");
+        printf("3:Search for a contact\n");
+        printf("4:Modify a contact\n");
+        printf("5:Delete a contact\n");
+        printf("6:Exit the program\n");
+
+        printf("\nChoose a number:");
+        scanf("%d", &choise);
+        switch (choise)
+        {
+        case 1:
+            Addnewcontact();
+            break;
+        case 2:
+            Viewcontact();
+            break;
+        default:
+            printf("Invalid choise\n");
+        }
+    }
+}
+
+int main()
+{
     Displaymenu();
-
     return 0;
 }
